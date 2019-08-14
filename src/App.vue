b4fff0aef1ab73a55a06eff2b8a7baf4137f63e4
Henri Delozanne <hnr.delozanne@gmail.com>
2019-08-14 19:07:16 +0200

    Created picture grid with Pinterest style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="menu-ctn-ctn">
+        <app-menu class="menu"/>
+      </div>
+      <app-main class="main"/>
+    <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">    
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Menu from './components/Menu.vue';
+import Main from './components/Main.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    'app-menu': Menu,
+    'app-main': Main,
+  },
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  background: rgb(24, 24, 24);
+  color: rgb(222, 222, 222);
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
 }
+
+.menu {
+  position: fixed;
+  width: 18%;
+  min-width: 240px;
+}
+
+.menu-ctn-ctn {
+  width: 18%;
+  min-width: 240px;
+  height: 100vh;
+}
+
+.main {
+  width: 82%;
+}
+
+@media screen and (min-width: 769px) {
+  .menu {
+    height: 100vh;
+  }
+
+  .menu-ctn-ctn {
+    height: 100vh;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  #app {
+    flex-direction: column;
+  }
+
+  .menu {
+    flex-basis: 70px;
+    height: 70px;
+    width: 100%;
+    background: rgb(24, 24, 24);
+  }
+
+  .menu-ctn-ctn {
+    width: 100%;
+    height: 70px;
+    background: rgb(24, 24, 24);
+  }
+
+  .main {
+  width: 100%;
+}
+}
+</style>
+
+<style lang="scss">
+@import 'src/style/main.scss';
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <div class="page-title">
+      <div>Charles Michel Photography</div>
+    </div>
+    <div class="menu-outter-ctn">
+      <div class="menu-inner-ctn desktop-view">
+        <div>Galeries</div>
+        <div>Contact</div>
+        <div>Bio</div>
+      </div>
+      <div class="menu-inner-ctn smartphone-view">
+        <i class="el-icon-menu"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Menu',
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .page-title {
+    flex-basis: 180px;
+  }
+
+  .menu-outter-ctn {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .menu-inner-ctn {
+    flex-basis: 400px;
+    width: 280px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .menu-inner-ctn div {
+    padding: 5px 25px;
+  }
+
+  .menu-inner-ctn div:hover {
+    cursor: pointer;
+    background: rgb(215, 214, 214);
+    color: rgb(24, 24, 24);
+  }
+
+  .smartphone-view {
+    display: none;
+  }
+
+
+  /* Trouver une meilleure façon de centrer */
+  i {
+    font-size: 1.7em;
+    margin-top: 22px;
+    margin-left: 22px;
+  }
+
+  .page-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  @media screen and (max-width:768px) {
+    .container {
+      flex-direction: row;
+    }
+
+    .menu-outter-ctn {
+      align-items: flex-end;
+    }
+
+    .menu-inner-ctn {
+      flex-basis: 70px;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      width: 70px;
+      margin-right: 25px;
+    }
+
+    .smartphone-view {
+      display: block;
+    }
+
+    .desktop-view {
+      display: none;
+    }
+
+    .page-title {
+      font-size: 0.7em;
+    }
+  }
+</style>
+
