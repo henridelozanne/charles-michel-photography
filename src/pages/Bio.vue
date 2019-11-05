09db6c3a8404b60ff284ac28b9e2d4f7b81a15dc
Henri Delozanne <hnr.delozanne@gmail.com>
2019-11-05 22:58:05 +0100

    Add picture in bio

--- src/pages/Bio.vue
+++ src/pages/Bio.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>Who am I ?</h1>
-    <p>Born in Auxerre, I spent my childhood with my family in Orleans and at the age of 19 I went to Lille to study business.
+    <p class="bio-content">
+      <img class="charles-picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz0HzRs9GtfLbIhN00Nee6_rWkoy7GEmgRtlfByn56JNCvswsX" alt="charles-michel">
+      <br>
+      Born in Auxerre, I spent my childhood with my family in Orleans and at the age of 19 I went to Lille to study business.
       <br>
       In 2012, I decided to embark on an epic 15,000-kilometer journey by bicycle from France through Europe, Russia and Asia to India. The whole journey taking a year to complete the circuit from Lille to Mumbai.
       <br>
@@ -9,11 +11,8 @@
       <br>
       It was also in Australia that I started to develop my passion for photography. During my last 6 months there I looked for resources and began training myself on the technical aspects, the rules of composition and editing.
       <br>
-      My photographic style was still being refined when I arrived in the Philippines. Alone in the streets of Manila I explored the city in every nook and cranny finding myself confronted with the harsh reality of life on the edges. However, at the same time I experienced beautiful meetings there discovering a people of kindness and generosity that I had not seen anywhere else. Very quickly I decided to focus my work on the inhabitants of Manila, on scenes of life and emotion.
-      <br>
-      Manila, which was originally just a place of passage on my way back to France, has become my place of life.
+      My photographic style was still being refined when I arrived in the Philippines. Alone in the streets of Manila I explored the city in every nook and cranny finding myself confronted with the harsh reality of life on the edges. However, at the same time I experienced beautiful meetings there discovering a people of kindness and generosity that I had not seen anywhere else. Very quickly I decided to focus my work on the inhabitants of Manila, on scenes of life and emotion. Manila, which was originally just a place of passage on my way back to France, has become my place of life.
     </p>
-    <img class="charles-picture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz0HzRs9GtfLbIhN00Nee6_rWkoy7GEmgRtlfByn56JNCvswsX" alt="charles-michel">
   </div>
 </template>
 
@@ -23,9 +22,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 .charles-picture {
   border-radius: 50%;
   border: solid;
+  /* display: inline-block; */
+  float: left;
+  margin: 25px;
+}
+
+.bio-content {
+  /* display: inline-block; */
+  float: left;
+  shape-outside: circle(50%);
+  /* width: 200px; */
+  height: 200px;
+  font-size: 1.1em;
+  width: 900px;
+
+  &span {
+    margin-bottom: 30px;
+  }
 }
 </style>
