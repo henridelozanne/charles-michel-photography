91eb3463c9ac4123d00102c29c520f0c68991b59
Henri Delozanne <hnr.delozanne@gmail.com>
2019-11-10 19:33:34 +0100

    Create contact page.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,35 +1,130 @@
 <template>
-  <div>
-    <h1>Contact Page</h1>
-    <img class="instagram-img" @click="goToInstagram()" src="../website-pictures/instagram-picture.svg" alt="">
+  <div class="container">
+    <h1>Contact me</h1>
+    <div class="horizontal-bar"></div>
+    <form action="">
+      <h2>Charles Michel</h2>
+      <p>07.12.42.33.89</p>
+      <p>Manilla, The Philippines</p>
+      <el-input v-model="form.name" class="black-input" placeholder="Name*"></el-input>
+      <el-input v-model="form.email" class="black-input" placeholder="Email*"></el-input>
+      <el-input v-model="form.phone" class="black-input" placeholder="Phone"></el-input>
+      <el-input v-model="form.subject" class="black-input" placeholder="Subject"></el-input>
+      <el-input v-model="form.message" type="textarea" class="black-input" placeholder="Message*"></el-input>
+      <el-button type="primary" @click="sendMail">Send</el-button>
+    </form>
+    <h1 class="follow-me">Follow me</h1>
+    <div class="horizontal-bar"></div>
+    <img class="instagram-img" @click="goToInstagram()" src="http://pluspng.com/img-png/instagram-png-instagram-png-logo-1455.png" width="82" height="86" alt="instagram">
   </div>
 </template>
 
 <script>
+import { Input, Button } from 'element-ui';
+
 export default {
   name: 'Contact',
+  components: {
+    'el-input': Input,
+    'el-button': Button,
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        phone: '',
+        subject: '',
+        message: '',
+      },
+    };
+  },
   methods: {
     goToInstagram() {
       var win = window.open('https://www.instagram.com/myshutterbag/?hl=fr', '_blank');
       win.focus();
+    },
+    sendMail() {
+      console.log('sendMail called');
     },
   },
 }
 </script>
 
 <style lang="scss">
+input {
+    color: black !important;
+}
+
+textarea {
+    color: black !important;
+    min-height: 200px !important;
+}
+</style>
+
+<style lang="scss" scoped>
+h1 {
+  margin-top: 50px;
+}
+
+.horizontal-bar {
+  width: 50%;
+  margin: auto;
+  height: 3px;
+  background: radial-gradient(#242424, rgb(0, 0, 0));
+}
+
+.horizontal-bar:last-of-type {
+  margin-bottom: 40px;
+}
+
 .instagram-img {
-  background: rgb(233, 233, 233);
-  width: 140px;
-  border-radius: 50%;
-  padding: 3px;
-  -webkit-transition: background-color .5s ease-out;
-  -moz-transition: background-color .5s ease-out;
-  -o-transition: background-color .5s ease-out;
-  transition: background-color .5s ease-out;
-  &:hover {
-    cursor: pointer;
-    background-color: rgb(68, 222, 176);
+  cursor: pointer;
+  width: 86px;
+}
+
+form {
+  width: 50%;
+  margin: 40px auto 0 auto;
+  min-width: 400px;
+
+
+  .el-input {
+    margin-bottom: 20px;
+
+    ::placeholder {
+      color: rgb(43, 43, 43);
+      opacity: 1;
+    }
+  }
+
+  .el-textarea {
+    margin-bottom: 20px;
+
+    ::placeholder {
+      color: rgb(43, 43, 43);
+      opacity: 1;
+    }
+  }
+
+  .el-button {
+    float: right;
+    font-family: 'caviar dreams' !important;
+  }
+}
+
+.follow-me {
+  clear: both;
+  margin-top: 150px;
+}
+
+.container {
+  text-align: center;
+  font-family: 'caviar dreams' !important;
+  margin-bottom: 320px;
+
+  img {
+    margin: auto;
   }
 }
 </style>
