b0e491e0f3005d75e510fc29e2cf9e0c6b0ae0bf
Henri Delozanne <hnr.delozanne@gmail.com>
2020-06-08 15:03:45 +0200

    Have clean admin side-bar.

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,9 +1,146 @@
 <template>
-  <h1>Admin page</h1>
+  <div class="admin">
+    <div class="page-wrapper default-theme sidebar-bg bg1 toggled">
+      <nav id="sidebar" class="sidebar-wrapper">
+        <div class="sidebar-content">
+          <div class="sidebar-brand">
+            <router-link to="/admin" class="company-name">
+              <span>Charles Michel<br>Photography</span>
+            </router-link>
+          </div>
+          <div class="sidebar-header">
+            <div class="user-pic">
+              <img class="img-responsive img-rounded" src="../img/avatar.png" alt="User picture">
+            </div>
+            <div class="user-info">
+              <span class="user-name">{{firstName}}
+                <strong>{{lastName}}</strong>
+              </span>
+              <span class="user-role"> {{email}} </span>
+              <span class="user-status">
+                <i class="fa fa-circle"></i>
+                <span>Online</span>
+              </span>
+            </div>
+          </div>
+          <div class="sidebar-menu">
+            <ul>
+              <li>
+                <router-link to="/admin">
+                  <i class="fa fa-home"></i>
+                  <span>Overview</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/admin/products">
+                  <i class="fa fa-tags"></i>
+                  <span>Products</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/admin/profile">
+                  <i class="fa fa-user"></i>
+                  <span>Profile</span>
+                </router-link>
+              </li>
+              <li>
+                  <a href="#" @click="logout()">
+                      <i class="fa fa-power-off"></i>
+                      <span>Logout</span>
+                  </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main class="page-content">
+          <router-view @logout="logout" :userFirstName="firstName"/>
+      </main>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </div>
+  </div>
 </template>
 
 <script>
+import '../assets/admin.scss';
+
+import { fb } from '../firebase';
+
 export default {
-  name: 'Admin',
-}
+  name: "admin",
+  data(){
+    return{
+      firstName: undefined,
+      lastName: undefined,
+      email: undefined,
+    };
+  },
+  methods:{
+    logout() {
+      fb.auth().signOut()
+        .then(() => {
+          this.$router.replace('/');
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    },
+  },
+  // created() {
+  //   const user = fb.auth().currentUser;
+  //   this.email = user.email;
+  //   db.collection('profiles').doc(user.uid)
+  //                 .get()
+  //                 .then(doc => {
+  //                   this.firstName = doc.data().firstName;
+  //                   this.lastName = doc.data().lastName;
+  //                 })
+  //                 .catch((err) => {
+  //                   console.log(err);
+  //                 })
+  // },
+};
 </script>
+
+<style lang="scss">
+.admin {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  .company-name {
+    text-align: center;
+  }
+
+  .sidebar-header {
+    display: flex;
+    align-items: center;
+    margin: 50px 0 90px;
+    border-color: #464646;
+    border-top: 1px solid #464646;
+    border-bottom: 1px solid #464646;
+
+    .user-pic {
+      align-self: flex-start;
+
+      img {
+        border-radius: 50%;
+        width: 45px;
+        height: 45px;
+      }
+    }
+
+    .user-info {
+      text-align: left;
+      margin-left: 20px;
+    }
+  }
+}
+
+.page-content {
+  height: 100%;
+  background-image: linear-gradient(to top, #dfe9f3 0%, white 100%);
+}
+
+.sidebar-brand {
+  margin-top: 30px;
+}
+</style>
