c07be9dedd7df980cecacc744a4b076144012ef1
Henri Delozanne <hnr.delozanne@gmail.com>
2020-07-21 17:55:53 +0200

    Handle responsive for new gallery page.

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="gallery">
     <div class="gallery-nav">
-      <img class="website-white-logo" src="https://res.cloudinary.com/charlesmichel-photography/image/upload/v1595342632/Website%20assets/Logo_blanc_pw4wgh.png" alt="website-logo-white">
-      <router-link to="/" class="go-back-container">
-        <goBackSvg />
-        <h2>{{ $t('Gallery.backToMainSite')}}</h2>
-      </router-link>
+      <div class="container">
+        <img class="website-white-logo" src="https://res.cloudinary.com/charlesmichel-photography/image/upload/v1595342632/Website%20assets/Logo_blanc_pw4wgh.png" alt="website-logo-white">
+        <router-link to="/" class="go-back-container">
+          <goBackSvg />
+          <h2>{{ $t('Gallery.backToMainSite')}}</h2>
+        </router-link>
+      </div>
     </div>
-    <iframe id='pixelsshoppingcartiframe' src='https://fineartamerica.com/widgetshoppingcart/artwork.html?memberidtype=artistid&memberid=929174&domainid=0&showheader=0&height=600&autoheight=true' style='display: inline-block; width: 100%; height: 100vh; border: none; overflow: hidden;'></iframe>
+    <iframe id='pixelsshoppingcartiframe'
+            src='https://fineartamerica.com/widgetshoppingcart/artwork.html?memberidtype=artistid&memberid=929174&domainid=0&showheader=0&height=600&autoheight=true'
+            class="container"
+            style='display: inline-block; width: 100%; height: 100vh; border: none; overflow: hidden;'></iframe>
   </div>
 </template>
 
@@ -29,30 +34,59 @@ export default {
   background: white;
   min-height: 100vh;
   color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   .gallery-nav {
-    height: 100px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 30px 50px;
     margin-bottom: 10px;
     box-shadow: 0 2px 10px rgba(103, 103, 103, 0.2);
     font-family: 'Raleway', sans-serif;
+    align-self: stretch;
 
-    .website-white-logo {
-      width: 200px;
-    }
-
-    .go-back-container {
+    .container {
+      height: 100px;
       display: flex;
-      color: black;
-      text-decoration: none;
+      justify-content: space-between;
+      align-items: center;
+      padding: 30px 50px;
 
-      svg {
-        margin-right: 10px;
+      .website-white-logo {
+        width: 200px;
+      }
+
+      .go-back-container {
+        display: flex;
+        align-items: center;
+        color: black;
+        text-decoration: none;
+
+        svg {
+          margin-right: 10px;
+        }
       }
     }
+
+    @media screen and (max-width: 500px) {
+      .container {
+        padding: 20px 0;
+
+        .go-back-container {
+          font-size: 10px;
+
+          svg {
+            margin-right: 5px;
+            width: 17px;
+          }
+        }
+      }
+    }
+  }
+
+  .container {
+    max-width: 1224px;
+    width: 92%;
+    margin: 0 auto;
   }
 }
 </style>
