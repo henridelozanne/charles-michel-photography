293bd11fceaa94309a9110ba4bd5c8cb72c65f4b
Henri Delozanne <hnr.delozanne@gmail.com>
2020-07-21 18:39:59 +0200

    Add full-page image in landing.

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="homepage">
+    <div class="landing-img-ctn">
+      <img :src="orderedList[0].image" alt="landing-image">
+    </div>
   </div>
 </template>
 
@@ -13,19 +16,48 @@ export default {
       itemList: [],
     };
   },
-  // mounted() {
-  //   db.collection('homepage_pictures').get().then((querySnapshot) => {
-  //     querySnapshot.forEach((doc) => {
-  //       const element = doc.data();
-  //     });
-  //   });
-  // },
+  computed: {
+    orderedList() {
+      function compare(a, b) {
+        if ( a.index < b.index ){
+          return -1;
+        } else if ( a.index > b.index ){
+          return 1;
+        }
+        return 0;
+      };
+      return this.itemList.sort(compare);
+    },
+  },
+  created() {
+    db.collection('homepage_pictures').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.itemList.push(doc.data());
+      });
+    });
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .homepage {
-  height: 100%;
+  height: 100vh;
   width: 100%;
+
+  .landing-img-ctn {
+    height: 100%;
+
+    img {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+
+@media screen and (max-width:768px) {
+  .homepage {
+    height: calc(100vh - 70px);
+  }
 }
 </style>
